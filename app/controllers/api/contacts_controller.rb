ed9f034be38f6229ba5585818b06dd7462ba3ca5
dani <dani.zaghian@gmail.com>
2020-01-23 10:41:50 -0800

    adds validators and happy/sad paths

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -16,8 +16,11 @@ class Api::ContactsController < ApplicationController
      latitude: params[:latitude],
      longitude: params[:longitude]
     )
-    @contact.save
-    render 'show.json.jb'
+    if @contact.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -26,20 +29,25 @@ class Api::ContactsController < ApplicationController
   end
 
   def update
-    coordinates = Geocoder.coordinates(params[:address])
     @contact = Contact.find(params[:id])
-    
+
+    if params[:address]
+      coordinates = Geocoder.coordinates(params[:address])
+      @contact.longitude = coordinates[1] || @contact.longitude
+      @contact.latitude = coordinates[0] || @contact.latitude
+    end
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.bio = params[:bio] || @contact.bio
-    @contact.longitude = coordinates[1] || @contact.longitude
-    @contact.latitude = coordinates[0] || @contact.latitude
 
-    @contact.save
-    render 'show.json.jb'
+    if @contact.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
